fb2c0562e0ddf7ab40b13e5b998145abf9bac2ad
neilsatt <neilsatt@gmail.com>
2018-06-02 09:27:47 -0600

    revert to PDF version of logintest

--- test/LoginTest.js
+++ test/LoginTest.js
@@ -1,23 +1,22 @@
+// filename: test/LoginTest.js
 'use strict';
 var webdriver = require('selenium-webdriver');
 var test = require('selenium-webdriver/testing');
-const { describe, it, before, after } = require('mocha');
-
 test.describe('Login', function() {
-    this.timeout(30000);
-    var driver;
-    test.beforeEach(function() {
-        var vendorDirectory = process.cwd() + '/vendor';
-        process.env.PATH = vendorDirectory + ":$PATH";
-        driver = new webdriver.Builder().forBrowser('firefox').build();
-    });
-    test.afterEach(function() {
-        //driver.quit();
-    });
+this.timeout(30000);
+var driver;
+test.beforeEach(function() {
+var vendorDirectory = process.cwd() + '/vendor';
+process.env.PATH = vendorDirectory + ":$PATH";
+driver = new webdriver.Builder().forBrowser('firefox').build();
+});
+test.afterEach(function() {
+driver.quit();
+});
 test.it('with valid credentials', function() {
-    driver.get('http://the-internet.herokuapp.com/login');
-    driver.findElement({id: 'username'}).sendKeys('tomsmith');
-    driver.findElement({id: 'password'}).sendKeys('SuperSecretPassword!');
-    driver.findElement({css: 'button'}).click();
-    });
+driver.get('http://the-internet.herokuapp.com/login');
+driver.findElement({id: 'username'}).sendKeys('tomsmith');
+driver.findElement({id: 'password'}).sendKeys('SuperSecretPassword!');
+driver.findElement({css: 'button'}).click();
+});
 });
